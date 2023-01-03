46e612d0b20d0a398baeae4884eef2aa9f2c7a2e
celophi <celophi@dakko.me>
2023-01-02 21:24:38 -0800

    Fixed spacing inconsistencies.

--- Zlib.Extended/Compressor.cs
+++ Zlib.Extended/Compressor.cs
@@ -3,18 +3,18 @@ using static Zlib.Extended.Zlib;
 
 namespace Zlib.Extended
 {
-	/// <summary>
-	/// Provides for a default wrapper around managing a stream and performing inflate/deflate.
-	/// </summary>
+    /// <summary>
+    /// Provides for a default wrapper around managing a stream and performing inflate/deflate.
+    /// </summary>
     public static class Compressor
-	{
-		/// <summary>
-		/// Inflates compressed input.
-		/// </summary>
-		/// <param name="input">Byte array of compressed data.</param>
-		/// <param name="output">Empty array to place decompressed data.</param>
-		/// <returns></returns>
-		public static ReturnCode Decompress(byte[] input, ref byte[] output)
+    {
+        /// <summary>
+        /// Inflates compressed input.
+        /// </summary>
+        /// <param name="input">Byte array of compressed data.</param>
+        /// <param name="output">Empty array to place decompressed data.</param>
+        /// <returns></returns>
+        public static ReturnCode Decompress(byte[] input, ref byte[] output)
         {
             var stream = new z_stream
             {
@@ -25,7 +25,7 @@ namespace Zlib.Extended
             };
 
             Inflate.inflateInit(stream);
-			return Inflate.inflate(stream, FlushValue.Z_FINISH);
+            return Inflate.inflate(stream, FlushValue.Z_FINISH);
         }
-	}
+    }
 }
--- Zlib.Extended/Enumerations/ReturnCode.cs
+++ Zlib.Extended/Enumerations/ReturnCode.cs
@@ -6,14 +6,14 @@
     /// </summary>
     public enum ReturnCode : int
     {
-		Z_OK = 0,
-		Z_STREAM_END = 1,
-		Z_NEED_DICT = 2,
-		Z_ERRNO = -1,
-		Z_STREAM_ERROR = -2,
-		Z_DATA_ERROR = -3,
-		Z_MEM_ERROR = -4,
-		Z_BUF_ERROR = -5,
-		Z_VERSION_ERROR = -6,
-	}
+        Z_OK = 0,
+        Z_STREAM_END = 1,
+        Z_NEED_DICT = 2,
+        Z_ERRNO = -1,
+        Z_STREAM_ERROR = -2,
+        Z_DATA_ERROR = -3,
+        Z_MEM_ERROR = -4,
+        Z_BUF_ERROR = -5,
+        Z_VERSION_ERROR = -6,
+    }
 }
