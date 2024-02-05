708baea0b6a091e8a9f1842b0d127e28a481be89
Chris Harris <csharris@protonmail.com>
2024-02-04 16:49:37 -0800

    Improve interface

---
+++ Zlib.Extended/IZipper.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Zlib.Extended;
+
+public interface IZipper
+{
+    /// <summary>
+    /// Decompresses compressed data. The returned array is the size specified.
+    /// </summary>
+    /// <param name="compressedData">Data that has previously been compressed by zlib.</param>
+    /// <param name="decompressedSize">Size of the output array.</param>
+    /// <returns>Array of decompressed data.</returns>
+    Task<byte[]> DecompressAsync(Stream compressedData, int decompressedSize);
+
+    /// <summary>
+    /// Decompresses compressed data. The returned array is the size specified.
+    /// </summary>
+    /// <param name="compressedData">Data that has previously been compressed by zlib.</param>
+    /// <param name="decompressedSize">Size of the output array.</param>
+    /// <returns>Array of decompressed data.</returns>
+    Task<byte[]> DecompressAsync(byte[] compressedData, int decompressedSize);
+}
---
+++ Zlib.Extended/Zipper.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Zlib.Extended.Enumerations;
+
+namespace Zlib.Extended;
+public class Zipper : IZipper
+{
+    /// <inheritdoc/>
+    public async Task<byte[]> DecompressAsync(Stream compressedData, int decompressedSize)
+    {
+        using var memoryStream = new MemoryStream();
+        compressedData.CopyTo(memoryStream);
+
+        return await this.DecompressAsync(memoryStream.ToArray(), decompressedSize);
+    }
+
+    /// <inheritdoc/>
+    public async Task<byte[]> DecompressAsync(byte[] compressedData, int decompressedSize)
+    {
+        var buffer = new byte[decompressedSize];
+
+        if (Compressor.Decompress(compressedData, ref buffer) != ReturnCode.Z_STREAM_END)
+        {
+            throw new InvalidDataException("Error. Unable to decompress data correctly.");
+        }
+
+        return await Task.FromResult(buffer);
+    }
+}
